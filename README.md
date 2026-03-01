c2262593ee9ab21006a899e3dbcd5613349f57d6
Joel Ting <joelccting@gmail.com>
2026-03-01 11:13:12 +0800

    Joel's first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pd2prj
 Final project for Program Design 2
+
+Joel Ting 414410068
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Joel Ting 414410068\n");
+    
+    return 0;
+}
