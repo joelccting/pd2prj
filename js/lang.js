a15d5963e2544f61f5abe596d1ce2fa9231aba8f
Joel Ting <joelccting@gmail.com>
2026-04-06 14:52:37 +0800

    modified:   index.html 	modified:   js/lang.js

--- js/lang.js
+++ js/lang.js
@@ -5,7 +5,13 @@ const translations = {
             "start_label": "Start Location:",
             "end_label": "End Location:",
             "algorithm_label": "Algorithm:",
-            "find_route_btn": "Find Route"
+            "find_route_btn": "Find Route",
+            "select_a_start_label": "Select a start location",
+            "select_an_end_label": "Select an end location",
+            "algorithm_select_label": "Choose an algorithm",
+            "algorithm_bfs_label": "BFS",
+            "algorithm_dfs_label": "DFS",
+            "algorithm_dik_label": "Dijkstra's Algorithm"
         }
     },
     zh: {
@@ -14,7 +20,13 @@ const translations = {
             "start_label": "起始位置：",
             "end_label": "終點位置：",
             "algorithm_label": "演算法：",
-            "find_route_btn": "尋找路線"
+            "find_route_btn": "尋找路線",
+            "select_a_start_label": "選擇一個起始位置",
+            "select_an_end_label": "選擇一個終點位置",
+            "algorithm_select_label": "選擇一個演算法：",
+            "algorithm_bfs_label": "廣度優先走訪",
+            "algorithm_dfs_label": "深度優先走訪",
+            "algorithm_dik_label": "戴克斯特拉演算法"
         }
     }
 };
